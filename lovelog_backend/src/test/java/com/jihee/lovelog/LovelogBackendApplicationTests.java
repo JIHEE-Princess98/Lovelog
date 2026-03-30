b4af2b5e96d31bbadd4517d849e066977287a5ed
JIHEE-Princess98 <kimjihee1113@gmail.com>
2026-03-30 13:45:42 +0900

    first commit: backend frontend init

---
+++ lovelog_backend/src/test/java/com/jihee/lovelog/LovelogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihee.lovelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovelogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
