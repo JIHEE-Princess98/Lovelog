8150d1180871d2d95dd1bba0bc554e3b84eee814
JIHEE-Princess98 <kimjihee1113@gmail.com>
2026-04-01 16:01:05 +0900

    feat: [FE] 우리의 기념일 , 추억쌓기 UI 생성

--- lovelog_frontend/next.config.ts
+++ lovelog_frontend/next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   devIndicators: false,
-  // output: "export",
-  // trailingSlash: true,
-  // basePath: isProd ? "/Lovelog" : "",
-  // assetPrefix: isProd ? "/Lovelog/" : "",
-  // images: {
-  //   unoptimized: true,
-  // },
   images: {
     unoptimized: true,
   },
